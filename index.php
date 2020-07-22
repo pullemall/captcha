aef49720d9ac3cd4e90f8ef3844429c6f4ec64b5
pullemall <61693872+pullemall@users.noreply.github.com>
2020-07-22 23:01:11 +0300

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
     <meta charset="UTF-8">
 </head>
 <body>
-    <form action="handler.php" method="POST">
-        <img src="captcha.php" alt="captcha" id="captcha-image1">
+    <form action="Handler.php" method="POST">
+        <img src="Captcha.php" alt="captcha" id="captcha-image1">
         <img src="ImgWord.php" alt="captcha" id="captcha-image">
         <br>
         <input type="text" name="code">
